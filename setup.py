bcd6770f2c621d7782c36e6ef0c79590607a67a7
sinisha <sinishadj@gmail.com>
2019-11-14 16:02:19 +0100

    release v_05 changes.

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_packages
 setup(
     name='ieUtils',  # How you named your package folder
     packages=find_packages(exclude=["ie_utils.test"]),  # Chose the same as "name"
-    version='0.4',  # Start with a small number and increase it with every change you make
+    version='0.5',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Utility functions',  # Give a short description about your library
     author='Sinisa Derasevic',  # Type in your name
     author_email='sinishadj@gmail.com',  # Type in your E-Mail
     url='https://github.com/sinishadj/ie-utils',  # Provide either the link to your github or to your website
-    download_url='https://github.com/sinishadj/ie-utils/archive/v_04.tar.gz',
+    download_url='https://github.com/sinishadj/ie-utils/archive/v_05.tar.gz',
     keywords=['UTILITIES', 'S3', 'DYNAMO DB'],  # Keywords that define your package best
     install_requires=[
         'boto3==1.9.43',
